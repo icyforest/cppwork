325a72d45b991cb5b6271826ce9f2bdc1e350f76
icyforest <2839962458@qq.com>
2025-11-02 16:30:08 +0800

    exchange files and proceed the project

--- cpp-training/src/Executorlmpl.cpp
+++ cpp-training/src/Executorlmpl.cpp
@@ -1,5 +1,6 @@
 #include "Command.hpp"
 #include "Executorlmpl.hpp"
+#include <unordered_map>
 namespace adas
 {
     Executor* Executor::NewExecutor(const Pose& pose)noexcept
@@ -13,29 +14,15 @@ namespace adas
 
     void Executorlmpl::Execute(const std::string& commands)noexcept
     {
-        for(const auto cmd : commands)
-        {
-            std::unique_ptr<ICommand> cmder;
-            if(cmd == 'F')
-            {
-                cmder = std::make_unique<FastCommand>();
-            }
-            else if(cmd == 'M')
-            {
-                cmder = std::make_unique<MoveCommand>();
-            }
-            else if(cmd == 'L')
-            {
-                cmder = std::make_unique<TurnLeftCommand>();
-            }
-            else if(cmd == 'R')
-            {
-                cmder = std::make_unique<TurnRightCommand>();
-            }
-
-            if(cmder)
-            {
-                cmder -> DoOperate(posehandler);
+        std::unordered_map<char, std::unique_ptr<ICommand>> cmderMap;
+        cmderMap.emplace('M', std::make_unique<MoveCommand>());
+        cmderMap.emplace('L', std::make_unique<TurnLeftCommand>());
+        cmderMap.emplace('R', std::make_unique<TurnRightCommand>());
+        cmderMap.emplace('F', std::make_unique<FastCommand>());
+        for (const auto cmd : commands) {
+            const auto it = cmderMap.find(cmd);
+            if (it != cmderMap.end()) {
+                it->second->DoOperate(posehandler);
             }
         }
     }
